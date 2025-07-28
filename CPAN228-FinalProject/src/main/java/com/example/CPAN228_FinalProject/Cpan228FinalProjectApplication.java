e227b3ca251c9456c83161b915a660b6e9c9a129
Russell2202 <n01426507@humbermail.ca>
2025-07-27 22:01:55 -0400

    Initial commit

---
+++ CPAN228-FinalProject/src/main/java/com/example/CPAN228_FinalProject/Cpan228FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CPAN228_FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpan228FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cpan228FinalProjectApplication.class, args);
+	}
+
+}
---
+++ CPAN228-FinalProject/src/test/java/com/example/CPAN228_FinalProject/Cpan228FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CPAN228_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cpan228FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
